f95090d7c5d598cf397a820ab8ffef822c2447ee
BaptisteSadoughi <115137893+BaptisteSadoughi@users.noreply.github.com>
2025-07-11 18:58:28 +0200

    Update fastq_processing.sh
    updated the call of base_path

--- Bioinformatic and R scripts/fastq_processing.sh
+++ Bioinformatic and R scripts/fastq_processing.sh
@@ -1,47 +1,56 @@
 #!/bin/bash
 
+# Define your base directory here
+base_path="/path/to/base_path"
+
+# Define subdirectories relative to base_path
+fastq_path="${base_path}/fastq"
+out_path="${base_path}/mapped"
+cov_out="${base_path}/cov"
+trim_path="${base_path}/trimmed"
+finalcov_out="${base_path}/final_cov"
+
+# Define genome folder path (update this to your genome folder)
+genome_path="/path/to/bismark/mmul10/"
+
+# Load required modules
 module load bowtie2-2.4.2-gcc-11.2.0
 module load samtools-1.13-gcc-11.2.0
 module load py-cutadapt-2.10-gcc-11.2.0
 
-fastq_path=${1}
+# Get the sample ID from the lids file based on SLURM_ARRAY_TASK_ID
+sampleID=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "${fastq_path}/lids")
 
-sampleID=$(sed -n ${SLURM_ARRAY_TASK_ID}p ${fastq_path}/lids)
-
-genome_path=/path/to/bismark/mmul10/
-out_path=/base_path/mapped
-cov_out=/base_path/cov
-trim_path=/base_path/trimmed
-finalcov_out=/base_path/final_cov
-
-# Create output directories if they don't exist
+# Create directories if they do not exist
 mkdir -p "${out_path}" "${cov_out}" "${trim_path}" "${finalcov_out}"
 
+# Add custom program paths to PATH if needed
 export PATH=$PATH:/path/to/programs/bin:/path/to/programs/TrimGalore-0.6.6:/path/to/programs/bismark2/Bismark-0.24.0
 
-## trim adaptors
-trim_galore --rrbs -j 8 -o ${trim_path} \
-    --basename ${sampleID} --gzip \
+# Trim adapters with trim_galore, flexible matching for R1 fastq files
+trim_galore --rrbs -j 8 -o "${trim_path}" \
+    --basename "${sampleID}" --gzip \
     ${fastq_path}/${sampleID}*R1*fastq.gz
 
-## map the reads
-bismark --genome_folder ${genome_path} \
-    -o ${out_path} \
+# Map reads with Bismark
+bismark --genome_folder "${genome_path}" \
+    -o "${out_path}" \
     --score_min L,0,-0.6 -R 10 \
-    --parallel 12 ${trim_path}/${sampleID}*.gz
+    --parallel 12 "${trim_path}/${sampleID}*.gz"
 
-## extract methylation data
-bismark_methylation_extractor -s -o ${cov_out} \
-        --gzip \
-        --bedGraph --comprehensive --parallel 12 \
-        --merge_non_CpG --genome_folder ${genome_path} \
-        ${out_path}/${sampleID}*.bam
+# Extract methylation data
+bismark_methylation_extractor -s -o "${cov_out}" \
+    --gzip \
+    --bedGraph --comprehensive --parallel 12 \
+    --merge_non_CpG --genome_folder "${genome_path}" \
+    "${out_path}/${sampleID}*.bam"
 
-## clean up unwanted files
-rm ${cov_out}/*${sampleID}*txt.gz
-rm ${cov_out}/*${sampleID}*bedGraph.gz
+# Clean up unwanted files
+rm "${cov_out}"/*"${sampleID}"*txt.gz
+rm "${cov_out}"/*"${sampleID}"*bedGraph.gz
 
-coverage2cytosine --merge_CpG --gzip --genome_folder $genome_path \
-        -o ${sampleID} \
-        --dir ${finalcov_out}/ \
-        ${cov_out}/${sampleID}*.cov.gz
+# Merge CpG coverage files into cytosine report
+coverage2cytosine --merge_CpG --gzip --genome_folder "${genome_path}" \
+    -o "${sampleID}" \
+    --dir "${finalcov_out}/" \
+    "${cov_out}/${sampleID}"*.cov.gz

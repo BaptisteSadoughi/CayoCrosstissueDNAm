965d53f6178a2a361e93badae09bc1f003459bbe
BaptisteSadoughi <115137893+BaptisteSadoughi@users.noreply.github.com>
2026-03-25 18:40:46 +0100

    Create MethylAgeDeviation_bodymass.R
    Creating the script. I removed the plotting part to simplify.

---
+++ Bioinformatic and R scripts/MethylAgeDeviation_bodymass.R
@@ -0,0 +1,115 @@
+#######################################################################
+#
+# Does weight adjusted for age predict clock residuals?
+#
+#######################################################################
+
+rm(list = ls())
+
+base_path <- "/path/to/project"  # <-- Define this path only once
+body_mass_data <- file.path(base_path,"metadata","MethylAgeDeviation_bodymass.txt")
+output_path <- file.path(base_path, "Figures")
+
+library_list <- c("lme4","car","DHARMa","RColorBrewer","ggpubr","dplyr","splines","jtools","interactions")
+lapply(library_list, require, character.only=TRUE)
+
+# Define ggplot theme upfront
+tissue_plot <- sort(c("whole blood","spleen","omental adipose","heart","kidney","lung","adrenal","thymus","thyroid", "pituitary", "liver", "skeletal muscle"))
+extended_palette <- setNames(colorRampPalette(brewer.pal(8, "Dark2"))(12),tissue_plot)
+
+# add missing tissues
+new_levels <- c("ovaries", "testis")
+
+# Define the new colors:
+new_colors <- c("#008B8B", "#4682B4")
+
+# Add these new levels and colors to your plots and palette
+tissue_plot <- c(tissue_plot, new_levels)
+extended_palette <- c(extended_palette, setNames(new_colors, new_levels))
+
+complete_data <- read.table(body_mass_data, sep = "txt", header = TRUE)
+
+# Test association with age deviation (sex-specific mixed-effects models)
+
+null <- lmer(Residual_adult ~ individual_sex + tissue + chronological_age + group + (1|monkey_id), data = complete_data, REML = F)
+
+mod_sex_int <- lmer(Residual_adult ~ bw_adj * (individual_sex + tissue) +
+                      chronological_age + group +
+                      (1 | monkey_id),
+                    data = complete_data,
+                    REML = FALSE
+                    )
+anova(mod_sex_int, null, test = "Chisq") #sig full-null
+
+drop1(mod_sex_int) #ns interaction bw_adj * sex & sig bw_adj * tissue
+
+mod_int_tissue <- lmer(Residual_adult ~ (bw_adj * tissue) + individual_sex + 
+                         chronological_age + group +
+                         (1 | monkey_id),
+                       data = complete_data,
+                       REML = FALSE
+                       )
+summary(mod_int_tissue)
+
+mod_bw <- lmer(Residual_adult ~ bw_adj + tissue + individual_sex +
+                 chronological_age + group +
+                 (1 | monkey_id),
+               data = complete_data,
+               REML = FALSE
+               )
+summary(mod_bw)
+
+anova(mod_int_tissue, mod_bw, test = "Chisq") #sig interaction
+anova(mod_bw, null, test = "Chisq") #sig for the main effect
+
+# MODEL DIAGNOSTICS
+vif(lm(Residual_adult ~ bw_adj + tissue + chronological_age + group + individual_sex, data = complete_data))
+
+simulationoutput <- DHARMa::simulateResiduals(fittedModel = mod_int_tissue, plot = F, re.form = NULL)
+residuals(simulationoutput, quantileFunction = qnorm, outlierValues = c(-7,7))
+plot(simulationoutput)
+
+## Plotting predicted values
+effect_plot(mod_bw, pred=bw_adj, interval=TRUE, plot.points=TRUE)
+interact_plot(mod_int_tissue, pred=bw_adj, modx= tissue, interval=T, plot.points=F, colors = extended_palette)
+
+
+#### Export model results
+library(flextable)
+# some renaming needed for the function
+boot.res.mod_int_tissue <- boot.res.int
+boot.res.mod_bw <- boot.res.bw
+
+format_model_result <- function(m){
+
+  model_name <- deparse(substitute(m))
+  conf_data <- get(paste0("boot.res.",model_name))
+
+  table_model_int <- as.data.frame(coef(summary(m)))
+  table_model_int <- cbind(table_model_int, conf_data$ci.estimates[,2:3])
+  table_model_int <- cbind(Variable = rownames(table_model_int), table_model_int)
+  rownames(table_model_int) <- NULL
+  names(table_model_int) <- c("Variable", "Estimate", "Std. Error", "df", "t-value", "p-value", "lowCI","highCI")
+  table_model_int = table_model_int %>%
+    mutate(Variable= dplyr::recode(Variable, "(Intercept)"="intercept",
+                                   "individual_sexM"="sex(m)",
+                                   "groupKK"="group(KK)"),
+           Variable = str_replace(Variable, "^bw_adj", "age-adjusted body mass")
+           )
+
+  # Round numeric columns for better formatting
+  table_model_int <- table_model_int %>%
+    mutate(across(c(Estimate, `Std. Error`, df, `t-value`, lowCI, highCI), ~ round(., 2)),
+           `p-value` = ifelse(`p-value` < 0.001, "<0.001", round(`p-value`, 2)))
+
+  # Create the table
+  ft_int <- flextable(table_model_int) %>%
+    theme_vanilla() %>%
+    autofit() %>%
+    bold(part = "header")
+
+  flextable::save_as_image(ft_int, paste0(output_path,"/bodymass_agedev_",model_name,"_.png"), zoom = 2)
+}
+
+format_model_result(mod_int_tissue)
+format_model_result(mod_bw)

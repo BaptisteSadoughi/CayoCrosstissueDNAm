30839197341a2a443c446f0653e69ee197ffe9b5
BaptisteSadoughi <115137893+BaptisteSadoughi@users.noreply.github.com>
2026-03-26 18:53:22 +0100

    Update Tissuemarkers_plot.R
    Final cleaning.

--- Bioinformatic and R scripts/Tissuemarkers_plot.R
+++ Bioinformatic and R scripts/Tissuemarkers_plot.R
@@ -6,10 +6,17 @@
 # and generates a heatmap for visualization.
 # ==============================================================================
 
+# === Clear workspace ===
 rm(list = ls())
 
+# === Load libraries ===
 library_list <- c("tidyverse","dplyr","tidyr","purrr","parallel","stringr","MatrixGenerics","svglite","gplots","RColorBrewer","pheatmap")
 lapply(library_list, require, character.only = TRUE)
+
+# === Paths ===
+base_path <- "/path/to/your/directory" # <-- set the path once
+
+figure_path <- file.path(base_path, "Figures")
 
 # === Recode tissue names for plotting ===
 recode_map <- c(
@@ -20,33 +27,18 @@ recode_map <- c(
 
 tissue_oi <- c("whole_blood","spleen","omental_at","heart","testis","ovaries","kidney","lung","adrenal","thymus","thyroid","pituitary","liver","skeletal_muscle")
 
-# Define ggplot theme upfront
+# === Color theme ===
 tissue_plot <- sort(c("whole blood","spleen","omental adipose","heart","kidney","lung","adrenal","thymus","thyroid", "pituitary", "liver", "skeletal muscle"))
 extended_palette <- setNames(colorRampPalette(brewer.pal(8, "Dark2"))(12),tissue_plot)
-
-# add missing tissues
 new_levels <- c("ovaries", "testis")
-
-# Define the new colors:
 new_colors <- c("#008B8B", "#4682B4")
-
-# Add these new levels and colors to your plots and palette
 tissue_plot <- c(tissue_plot, new_levels)
 extended_palette <- c(extended_palette, setNames(new_colors, new_levels))
 
-# === Paths ===
-base_path <- "/path/to/your/directory" # <-- set the path once
-
-figure_path <- file.path(base_path, "Figures")
-
-# Load the concatenated list of tissue markers from Table S4
+# === Load data (concatenated list of tissue markers from Table S4)
 tissue_markers <- readxl::read_excel(paste0(base_path, "/SupplementaryTables.xlsx"), sheet = "TableS4")
 
 tissue_markers$marker <- ifelse(tissue_markers$mean_beta>0,"hyper","hypo")
-
-### NOTE: given that gonads were run independently it is possible that markers are common to other tissues.
-### To avoid cross-assignments, we excluded these markers associated with several tissues if they are 
-### hypo in both, or hyper in both.
 
 # Do the strongest markers tend to be hypo/hyper/equally distributed across tissues?
 strongest_250 <- tissue_markers_all %>% group_by(sig_tissue) %>% slice_max(abs(mean_beta), n=250, with_ties = TRUE) %>% ungroup()
@@ -99,25 +91,28 @@ top_mat_combined <- top_mat_combined %>%
   column_to_rownames("loci") %>%       # Use the 'loci' column as row names
   as.matrix()
 
-### Calculate the average percent methylation for each locus
+# === Calculate the average percent methylation for each locus ===
+
 rowMeans_meth <- rowMeans(top_mat_combined, na.rm = TRUE)
 
-### Combine with the `tissue_focal` and `marker` information
+# === Combine with the `tissue_focal` and `marker` information ===
+
 top_markersnoX <- top_markers %>% filter(!grepl("Region_X_*",sites)) 
+
 loci_to_tissue_focal <- top_markersnoX[, c("sites", "tissue", "marker")] %>%
   mutate(avg_meth = rowMeans_meth[match(sites, rownames(top_mat_combined))])
 
-### Order the rows by `tissue_focal` and decreasing average methylation within those levels
+# Order the rows by `tissue_focal` and decreasing average methylation within those levels
 loci_to_tissue_focal <- loci_to_tissue_focal %>%
   arrange(tissue, -avg_meth)
 
-### Get the ordered row names
+# Get the ordered row names
 ordered_rows <- loci_to_tissue_focal$sites
 
-### Reorder the rows of `top_mat_combined`
+# Reorder the rows of `top_mat_combined`
 top_mat_combined_ordered <- top_mat_combined[ordered_rows, ]
 
-### Reorder the columns based on tissue levels (already done but included for completeness)
+# Reorder the columns based on tissue levels (already done but included for completeness)
 tissue_levels <- unique(sub("_.*", "", colnames(top_mat_combined_ordered)))
 sorted_col_indices <- unlist(sapply(tissue_levels, function(tl) which(startsWith(colnames(top_mat_combined_ordered), tl))))
 top_mat_combined_ordered <- top_mat_combined_ordered[, sorted_col_indices]

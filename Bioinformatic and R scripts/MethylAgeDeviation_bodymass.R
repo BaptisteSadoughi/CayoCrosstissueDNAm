14f8eb8af52991d70a79c83a88a7127b844d0e76
BaptisteSadoughi <115137893+BaptisteSadoughi@users.noreply.github.com>
2026-03-26 14:15:44 +0100

    Update MethylAgeDeviation_bodymass.R
    Final cleaning.

--- Bioinformatic and R scripts/MethylAgeDeviation_bodymass.R
+++ Bioinformatic and R scripts/MethylAgeDeviation_bodymass.R
@@ -1,80 +1,88 @@
-#######################################################################
-#
-# Does weight adjusted for age predict clock residuals?
-#
-#######################################################################
+# ==============================================================================
+# Does weight adjusted for age predict DNAm clock deviations?
+# ==============================================================================
 
+# === Clear workspace ===
 rm(list = ls())
 
-base_path <- "/path/to/project"  # <-- Define this path only once
-body_mass_data <- file.path(base_path,"metadata","MethylAgeDeviation_bodymass.txt")
-output_path <- file.path(base_path, "Figures")
-
+# === Load libraries ===
 library_list <- c("lme4","car","DHARMa","RColorBrewer","ggpubr","dplyr","splines","jtools","interactions")
 lapply(library_list, require, character.only=TRUE)
 
-# Define ggplot theme upfront
+# === Paths ===
+base_path <- "/path/to/project"  # <-- Define this path only once
+
+body_mass_data <- file.path(base_path,"metadata","MethylAgeDeviation_bodymass.txt")
+output_path <- file.path(base_path, "output")
+
+# === Color theme ===
 tissue_plot <- sort(c("whole blood","spleen","omental adipose","heart","kidney","lung","adrenal","thymus","thyroid", "pituitary", "liver", "skeletal muscle"))
 extended_palette <- setNames(colorRampPalette(brewer.pal(8, "Dark2"))(12),tissue_plot)
-
-# add missing tissues
 new_levels <- c("ovaries", "testis")
-
-# Define the new colors:
 new_colors <- c("#008B8B", "#4682B4")
-
-# Add these new levels and colors to your plots and palette
 tissue_plot <- c(tissue_plot, new_levels)
 extended_palette <- c(extended_palette, setNames(new_colors, new_levels))
 
+# --------------------------------------------------------------------------
+# === LINEAR REGRESSION OF BODY MASS ON AGE DEVIATIONS CORRECTED FOR AGE ===
+# --------------------------------------------------------------------------
+
+# === Load data ===
 complete_data <- read.table(body_mass_data, sep = "txt", header = TRUE)
 
-# Test association with age deviation (sex-specific mixed-effects models)
+## Test association with age deviation (sex-specific mixed-effects models)
 
+# null model
 null <- lmer(Residual_adult ~ individual_sex + tissue + chronological_age + group + (1|monkey_id), data = complete_data, REML = F)
 
+# full model
 mod_sex_int <- lmer(Residual_adult ~ bw_adj * (individual_sex + tissue) +
                       chronological_age + group +
                       (1 | monkey_id),
                     data = complete_data,
                     REML = FALSE
                     )
-anova(mod_sex_int, null, test = "Chisq") #sig full-null
+
+anova(mod_sex_int, null, test = "Chisq") #sig full-null comparison
 
 drop1(mod_sex_int) #ns interaction bw_adj * sex & sig bw_adj * tissue
 
+# simplified model
 mod_int_tissue <- lmer(Residual_adult ~ (bw_adj * tissue) + individual_sex + 
                          chronological_age + group +
                          (1 | monkey_id),
                        data = complete_data,
                        REML = FALSE
                        )
+
 summary(mod_int_tissue)
 
+# model without interaction terms
 mod_bw <- lmer(Residual_adult ~ bw_adj + tissue + individual_sex +
                  chronological_age + group +
                  (1 | monkey_id),
                data = complete_data,
                REML = FALSE
                )
+
 summary(mod_bw)
 
+# === Model comparison ===
 anova(mod_int_tissue, mod_bw, test = "Chisq") #sig interaction
 anova(mod_bw, null, test = "Chisq") #sig for the main effect
 
-# MODEL DIAGNOSTICS
+# === Model diagnostics ===
 vif(lm(Residual_adult ~ bw_adj + tissue + chronological_age + group + individual_sex, data = complete_data))
 
 simulationoutput <- DHARMa::simulateResiduals(fittedModel = mod_int_tissue, plot = F, re.form = NULL)
 residuals(simulationoutput, quantileFunction = qnorm, outlierValues = c(-7,7))
 plot(simulationoutput)
 
-## Plotting predicted values
+# === Plotting predicted values ===
 effect_plot(mod_bw, pred=bw_adj, interval=TRUE, plot.points=TRUE)
 interact_plot(mod_int_tissue, pred=bw_adj, modx= tissue, interval=T, plot.points=F, colors = extended_palette)
 
-
-#### Export model results
+# === Export model results (Tables S21) ===
 library(flextable)
 # some renaming needed for the function
 boot.res.mod_int_tissue <- boot.res.int
